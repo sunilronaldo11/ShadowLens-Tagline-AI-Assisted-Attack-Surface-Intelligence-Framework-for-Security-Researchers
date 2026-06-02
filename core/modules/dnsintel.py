bb9a5c8431628181ca670fa9eef39386fd72b9c3
sunilronaldo11 <sharmaharshil043@gmail.com>
2026-06-02 23:41:10 +0530

    Delete dnsintel.py

--- core/modules/dnsintel.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-class DNSIntel:
-    def run(self, target, inventory):
-        try:
-            inventory.add("ip", socket.gethostbyname(target))
-        except:
-            inventory.add("ip", "unknown")
