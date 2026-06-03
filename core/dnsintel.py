f3fa20869e55c91bc7fc8dcf537988a2960add2c
sunilronaldo11 <sharmaharshil043@gmail.com>
2026-06-03 09:04:50 +0530

    Create dnsintel.py

---
+++ core/dnsintel.py
@@ -0,0 +1,10 @@
+# modules/dnsintel.py
+
+import socket
+
+class DNSIntel:
+    def run(self, target, inventory):
+        try:
+            inventory.add("ip", socket.gethostbyname(target))
+        except:
+            inventory.add("ip", "unknown")
